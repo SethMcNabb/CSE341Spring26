2e407acb55b427e3f81dc0b52d1b55126f61d979
Seth McNabb <sethmcnabb00@gmail.com>
2026-05-16 23:38:28 -0600

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
+const swaggerUi = require("swagger-ui-express");
 
 const routes = require("./routes");
 const swaggerSpec = require("./swagger");
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/", routes);
 
 async function startServer() {
